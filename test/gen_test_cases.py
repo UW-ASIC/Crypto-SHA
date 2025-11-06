194c08fe4ee0fee75c4f2752682bbc562c8074c1
judywsl <j542yu@uwaterloo.ca>
2025-11-06 14:55:52 -0500

    Fix bug of schedule words exceeding 32 bits
    * Truncate any exceeding bits by using bitwise
      AND with 0xffffffff

--- test/gen_test_cases.py
+++ test/gen_test_cases.py
@@ -15,6 +15,8 @@ def sigma1(w_2):
 def store_in_file(file, input, expected):
     json.dump({"input": input, "expected": expected}, file, indent=2)
 
+def to_hex(list):
+    return [f"{num:x}" for num in list]
 
 def compute_message_schedule(block):
     schedule = [None for i in range(64)]
@@ -23,7 +25,8 @@ def compute_message_schedule(block):
         schedule[i] = block[i]
     
     for i in range(16, 64):
-        schedule[i] = sigma0(schedule[i - 15]) + schedule[i - 7] + sigma1(schedule[i - 2]) + schedule[i - 16]
+        schedule[i] = (sigma0(schedule[i - 15]) + schedule[i - 7] + sigma1(schedule[i - 2]) + schedule[i - 16])\
+            & 0xffffffff # Truncate when exceeding one byte in size
 
     return schedule
 
@@ -35,7 +38,9 @@ def generate_test_cases():
         block = [random.getrandbits(32) for i in range(16)] # 512-bit input message
 
         with open(f"test_cases/test_case_{i+1}.json","w") as file:
-            store_in_file(file, block, compute_message_schedule(block))
+            # Convert to hexadecimal strings for easy debugging
+            # when comparing waveform values to test_case JSON files
+            store_in_file(file, to_hex(block), to_hex(compute_message_schedule(block)))
 
 if __name__ == "__main__":
     generate_test_cases()
