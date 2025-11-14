c8b04f15ffe6162764aff62793b53faf2009e4ce
parkwilliamy <parkwilliamy@gmail.com>
2025-11-14 16:59:38 -0500

    finished writing tests and module for padder, tests all pass

---
+++ src/padder.v
@@ -0,0 +1,215 @@
+module padder(
+  input  wire         clk,
+  input  wire         rst_n,
+
+  // Ingress bytes for exactly one message
+  input  wire         in_valid,
+  output wire         in_ready,
+  input  wire [7:0]   in_data,
+  input  wire         in_last,       // marks last byte of the message
+
+  // 512-bit block out
+  output reg          blk_valid,
+  input  wire         blk_ready,
+  output reg  [511:0] blk_data,
+
+  // Optional: message length out (debug)
+  output reg  [63:0]  bitlen
+);
+
+    reg [63:0] msgidx; // used for indexing blk_q after initial msg length is determined
+    reg [63:0] msglen; // length of input data
+    reg [1023:0] blk_q; // stores up to 2 blocks after padding
+    reg inter_ready; // intermediate signal to allow comb. logic with in_ready
+
+    assign in_ready = inter_ready;
+
+    reg [3:0] current_state, next_state;
+    localparam IDLE = 3'b000, 
+               MSG = 3'b001, // receive data bytes
+               PAD_0x80 = 3'b010, // pads first 0x80 byte
+               PAD_ZEROS = 3'b011, // pads 0x00 until 64 bits remain in block
+               BLK_DOUBLE = 3'b100, // shifts in first out of two blocks
+               BLK_SINGLE = 3'b101; // shifts in last block out of 2 blocks OR shifts in single block
+
+    assign bitlen = msglen;
+
+    always @ (posedge clk or negedge rst_n) begin
+
+        if (!rst_n) begin
+
+            blk_q <= 1024'b0;
+            msgidx <= 64'b0;
+            msglen <= 64'b0;
+            current_state <= IDLE;
+
+        end
+        
+        else begin
+
+            current_state <= next_state;
+
+            if (current_state == IDLE) begin
+
+                blk_q <= 1024'b0;
+                msgidx <= 64'b0;
+                msglen <= 64'b0;
+                
+            end
+
+
+            if (current_state == MSG) begin
+                
+                if (in_valid) begin // for non empty messages
+                    blk_q <= {blk_q[1015:0], in_data};
+                    msgidx <= msgidx+8;
+                end
+
+
+            end
+
+            else if (current_state == PAD_0x80) begin
+
+                msglen <= msgidx;
+                blk_q <= {blk_q[1015:0], 8'h80};
+                msgidx <= msgidx+8;
+
+            end
+
+            else if (current_state == PAD_ZEROS) begin
+
+                if (msgidx%512 == 448) begin
+                    blk_q <= {blk_q[959:0], msglen[63:0]};
+                end
+                else begin
+                    blk_q <= {blk_q[1015:0], 8'h00};
+                    msgidx <= msgidx+8;
+                end
+
+            end
+
+        end
+
+    end
+
+    // STATE TRANSITION LOGIC
+
+    always @ (*) begin
+
+        case (current_state) 
+
+            IDLE: begin
+                
+                if (!in_valid) begin
+                    if (in_last) next_state = BLK_SINGLE; // for empty messages
+                    else next_state = IDLE;
+                end
+                else next_state = MSG;
+
+            end
+
+            MSG: begin
+
+                if (in_valid) begin
+                
+                    if (!in_last) next_state = MSG;
+                    else next_state = PAD_0x80;
+
+                end
+
+                else next_state = IDLE;
+
+
+            end
+
+            PAD_0x80: begin
+
+                next_state = PAD_ZEROS;
+
+            end
+
+            PAD_ZEROS: begin
+
+                if (msgidx%512 == 448) begin
+                    if (msgidx == 448) next_state = BLK_SINGLE;
+                    else next_state = BLK_DOUBLE;
+                end
+                else next_state = PAD_ZEROS;
+
+            end
+
+            BLK_DOUBLE: begin
+
+                if (!blk_ready) next_state = BLK_DOUBLE;
+                else next_state = BLK_SINGLE;
+
+            end
+
+            BLK_SINGLE: begin
+
+                if (!blk_ready) next_state = BLK_SINGLE;
+                else next_state = IDLE;
+
+            end
+
+        endcase
+
+    end
+
+    // OUTPUT LOGIC
+
+    always @ (*) begin
+
+        case (current_state) 
+
+            IDLE: begin
+
+                inter_ready = 1;
+                blk_valid = 0;
+                blk_data = 0;
+
+            end
+
+            MSG: begin
+
+                if (in_last) inter_ready = 0;
+            
+            end
+
+            BLK_DOUBLE: begin
+
+                blk_valid = 1;
+
+                if (blk_ready) blk_data = blk_q[1023:512];
+                    
+            end
+
+            BLK_SINGLE: begin
+
+                blk_valid = 1;
+
+                if (blk_ready) begin
+
+                    if (!in_valid && in_last) blk_data = 1<<511;
+
+                    else begin
+                
+                        if (msgidx > 512) begin
+                            if (blk_q[511:0] == 512'b0) blk_valid = 0;
+                            else blk_data = blk_q[511:0];
+                        end
+                        else blk_data = blk_q[511:0];
+
+                    end
+
+                end
+            
+            end
+
+
+
+        endcase
+
+    end
+
+endmodule
---
+++ test/test_padder.py
@@ -0,0 +1,185 @@
+import cocotb
+import random
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge
+from cocotb.triggers import Timer, First
+from cocotb.types import Logic
+from cocotb.types import LogicArray
+from cocotb.types import Range
+
+
+async def send_message(dut, msg): # msg is a binary string
+    
+    msg_size = 0
+    data = [] # data[0] is blk1 data, data[1] is blk2 data (if it exists)
+    dut.in_last.value = 0
+    dut.in_valid.value = 0
+    msg_size = int(len(msg)/8)
+
+    # await for in_ready before transmitting data
+    while not dut.in_ready.value:
+        await RisingEdge(dut.clk)
+
+    # feed in data bytes every clock edge
+    for i in range(msg_size):
+
+        dut.in_valid.value = 1
+
+        await RisingEdge(dut.clk)
+        dut.in_data.value = LogicArray(msg[8*i:8*i+8])
+
+        if i == msg_size - 1:
+            dut.in_last.value = 1
+    
+    # edge case for empty messages
+    if msg_size == 0:
+        dut.in_last.value = 1
+        
+    await RisingEdge(dut.clk)
+    dut.blk_ready.value = 1
+
+    # wait until device is ready to accept blocks
+    while not dut.blk_valid.value:
+        await RisingEdge(dut.clk)
+
+    dut.in_valid.value = 0
+    dut.in_last.value = 0
+
+    data.append(dut.blk_data.value) # read in first block
+    await RisingEdge(dut.clk)
+    if dut.blk_valid.value == Logic(1): # read in second block if remaining data not empty
+        data.append(dut.blk_data.value)
+
+    return data
+
+def reference_padder(msg):
+
+    padded_msg = msg+"10000000"
+    padded_msg_array = []
+    
+    while len(padded_msg) % 512 != 448:
+        padded_msg += "0"*8
+        
+    
+    padded_msg += format(len(msg), "064b")
+
+    if len(padded_msg) == 512:
+        padded_msg_array.append(int(padded_msg[0:512],2))
+
+    else:
+        padded_msg_array = [int(padded_msg[0:512],2), int(padded_msg[512:1024],2)]
+    
+    return padded_msg_array
+    
+def randomize_message(length = 0):
+
+    if length == 0:
+        rand_length = random.choice(range(0, 953, 8))
+    else:
+        rand_length = length
+    return ''.join(random.choice("01") for _ in range(rand_length))
+    
+    
+
+@cocotb.test()
+async def test_padder(dut):
+    dut._log.info("Starting test for padder") # Log the start of the test
+    
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())  # Start the clock
+    
+    dut.rst_n.value = 0
+    dut.in_last.value = 0
+    dut.in_valid.value = 0
+    dut.in_data.value = 0
+    dut.in_last.value = 0
+    dut.blk_ready.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+
+    ''' FUNCTIONAL REQUIREMENTS
+
+    1) If bitlen = 0, blk_data = 1 << 511
+    2) If 0 < bitlen <= 440, last 64 bits of first/only block are msglen
+    3) If 448 <= bitlen <= 952, last 64 bits of second block are msglen
+    4) 0 < bitlen <= 952
+    5) No data is processed until in_ready = 1
+    6) in_valid signals empty messages
+    7) blk_valid = 0 after all blocks are transmitted
+    
+    '''
+
+    dut._log.info("Edge Cases")
+
+    dut._log.info("0 bit length message before messages have been padded") 
+    msg = ""
+    data = await send_message(dut, msg)
+    assert int(data[0]) == reference_padder(msg)[0], print(f"Expected (1 << 511), got {int(data[0])}")
+    
+    dut._log.info("440 bit length integer message")
+    msg = "1"*440
+    data = await send_message(dut, msg)
+    assert int(data[0]) == reference_padder(msg)[0], print(f"Expected (441 1s, 7 0s, and 64 bit rep of 440), got {int(data[0])}")
+    
+    dut._log.info("448 bit length integer message")
+    msg = "1"*448
+    data = await send_message(dut, msg)
+    assert int(data[0]) == reference_padder(msg)[0], print(f"Expected (449 1s, 63 0s), got {int(data[0])}")
+    assert int(data[1]) == reference_padder(msg)[1], print(f"Expected (448 0s, 64 bit rep of 448), got {int[data[1]]}")
+
+    dut._log.info("512 bit length integer message")
+    msg = "1"*512
+    data = await send_message(dut, msg)
+    assert int(data[0]) == reference_padder(msg)[0], print(f"Expected (512 1s), got {int(data[0])}")
+    assert int(data[1]) == reference_padder(msg)[1], print(f"Expected (1 1, 447 0s, 64 bit rep of 512), got {int[data[1]]}")
+
+    dut._log.info("952 bit length integer message")
+    msg = "1"*952
+    data = await send_message(dut, msg)
+    assert int(data[0]) == reference_padder(msg)[0], print(f"Expected (512 1s), got {int(data[0])}")
+    assert int(data[1]) == reference_padder(msg)[1], print(f"Expected (441 1s, 7 0s, and 64 bit rep of 952), got {int(data[0])}")
+
+    dut._log.info("0 bit length message after all messages have been padded") 
+    msg = ""
+    data = await send_message(dut, msg)
+    assert int(data[0]) == reference_padder(msg)[0], print(f"Expected (1 << 511), got {int(data[0])}")
+
+    dut._log.info("Randomized Tests")
+
+    for i in range(20):
+        msg = randomize_message(440)
+        data = await send_message(dut, msg)
+        assert int(data[0]) == reference_padder(msg)[0], print(f"Expected {reference_padder(msg)[0]}, got {int(data[0])}")
+        if len(data) == 2:
+            assert int(data[1]) == reference_padder(msg)[1], print(f"Expected {reference_padder(msg)[1]}, got {int(data[1])}")
+
+    for i in range(20):
+        msg = randomize_message(448)
+        data = await send_message(dut, msg)
+        assert int(data[0]) == reference_padder(msg)[0], print(f"Expected {reference_padder(msg)[0]}, got {int(data[0])}")
+        if len(data) == 2:
+            assert int(data[1]) == reference_padder(msg)[1], print(f"Expected {reference_padder(msg)[1]}, got {int(data[1])}")
+
+    for i in range(20):
+        msg = randomize_message(512)
+        data = await send_message(dut, msg)
+        assert int(data[0]) == reference_padder(msg)[0], print(f"Expected {reference_padder(msg)[0]}, got {int(data[0])}")
+        if len(data) == 2:
+            assert int(data[1]) == reference_padder(msg)[1], print(f"Expected {reference_padder(msg)[1]}, got {int(data[1])}")
+
+    for i in range(20):
+        msg = randomize_message(952)
+        data = await send_message(dut, msg)
+        assert int(data[0]) == reference_padder(msg)[0], print(f"Expected {reference_padder(msg)[0]}, got {int(data[0])}")
+        if len(data) == 2:
+            assert int(data[1]) == reference_padder(msg)[1], print(f"Expected {reference_padder(msg)[1]}, got {int(data[1])}")
+
+    for i in range(20):
+        msg = randomize_message()
+        data = await send_message(dut, msg)
+        assert int(data[0]) == reference_padder(msg)[0], print(f"Expected {reference_padder(msg)[0]}, got {int(data[0])}")
+        if len(data) == 2:
+            assert int(data[1]) == reference_padder(msg)[1], print(f"Expected {reference_padder(msg)[1]}, got {int(data[1])}")
+   
+    dut._log.info("Tests finished!")
